2ed5349276dd4cbf92b2d4570d5b89098d245d7c
Solejay <prj960827@gmail.com>
2019-10-28 10:07:44 +0800

    代码上传

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Solejay-code
 Renjie Pu's code
+
+### load_file.py
+
+读取训练集和测试集数据，处理后保存每个文件对应的 `label` 和 `拼接的 api 序列` 并保存为 `pkl` 文件实现数据的持久存储。
+
+[代码地址]()
+
+[学习笔记]()
+
+### train.py
+
+用 ngram 提取 api 序列特征，采用五折交叉验证用 xgboost 进行分类
+
+[代码地址]()
+
+### 提交结果
+
+- 词袋模型+xgboost 5折交叉验证
+
+unigram：0.567571（ngram_range=(1, 1)）
+
+3_gram：0.473048（ngram_range=(1, 3)）
+
+5_gram：0.473122（ngram_range=(1, 5)）
+
+- TFIDF模型+xgboost 5折交叉验证
+
+unigram：0.659438（ngram_range=(1, 1)）
+
+3_gram：0.505826（ngram_range=(1, 3)）
+
+5_gram：0.507149（ngram_range=(1, 5)）
+
---
+++ load_file.py
@@ -0,0 +1,54 @@
+'''
+实现读取 security_train.csv 和 security_test.csv 文件
+将文件按照 file_id 分组后
+按照 tid 和 index 排序
+分别用 labels 存放 file_id 对应的程序类别
+和    files  存放 file_id 包含的 api 序列
+最后导出 pkl 文件
+'''
+import pandas as pd
+import pickle
+import numpy as np
+
+train_path = r'security_train.csv'
+test_path = r'security_test.csv'
+
+
+def read_train_file(path):
+    labels = []
+    files = []
+    data = pd.read_csv(path)  
+    group_fileid = data.groupby('file_id')
+    for file_name, file_group in group_fileid:
+        print(file_name)  # 一共 13887 个文件
+        file_labels = file_group['label'].values[0]  # 属于哪一类病毒
+        result = file_group.sort_values(['tid', 'index'], ascending=True)  # 按照 tid 和 index 升序排列
+        api_sequence = ' '.join(result['api'])  # 把 api 拼接起来
+        labels.append(file_labels)
+        files.append(api_sequence)
+    labels = np.asarray(labels)  # 转换为 numpy 数组
+    with open("security_train.csv.pkl", 'wb') as f:
+        pickle.dump(labels, f)
+        pickle.dump(files, f)
+
+
+def read_test_file(path):
+    names = []
+    files = []
+    data = pd.read_csv(path)
+    group_fileid = data.groupby('file_id')
+    for file_name, file_group in group_fileid:
+        print(file_name)  # 一共 12955 个文件
+        result = file_group.sort_values(['tid', 'index'], ascending=True)
+        api_sequence = ' '.join(result['api'])
+        names.append(file_name)
+        files.append(api_sequence)
+    with open("security_test.csv.pkl", 'wb') as f:
+        pickle.dump(names, f)
+        pickle.dump(files, f)
+
+print("read train file.....")
+read_train_file(train_path)
+print("read test file......")
+read_test_file(test_path)
+
