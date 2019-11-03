ba08e3711952d010b3f0535600d62f94f22ca236
Solejay <prj960827@gmail.com>
2019-11-03 21:52:18 +0800

    添加xgboost.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-'''
-svm 分类
-'''
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
 import xgboost as xgb
 import pickle
 import numpy as np
@@ -12,99 +10,53 @@ import time
 import csv
 
 # test 文件
-with open("../test/security_test.csv.pkl", "rb") as f:
+with open("security_test.csv.pkl", "rb") as f:
     file_names = pickle.load(f)
     outfiles = pickle.load(f)  
 
 # train 文件
-with open("../test/security_train.csv.pkl", "rb") as f:
+with open("security_train.csv.pkl", "rb") as f:
     labels = pickle.load(f)
     files = pickle.load(f)  
 
 # 词袋模型 ngram 提取特征
 vectorizer = CountVectorizer(ngram_range=(1, 3))
-x_train = vectorizer.fit_transform(files)  
-x_test = vectorizer.transform(outfiles)  
-y_train = labels
+x = vectorizer.fit_transform(files)  # (13887, 180858)
+y = labels # (13887,)
+x_test = vectorizer.transform(outfiles)  # (12955, 180858)
 
-# # Tfidf 提取特征
-# vectorizer = TfidfVectorizer(ngram_range=(1,3), min_df=3, max_df=0.9, )  
-# train_features = vectorizer.fit_transform(files)  # (144, 19882)
-# out_features = vectorizer.transform(outfiles)  # (172, 19882)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_test, y_test)
+dout = xgb.DMatrix(x_test)
 
-# print(y_train)
+param = {'eval_metric': 'mlogloss', 'objective': 'multi:softprob',
+         'num_class': 8}  # 参数
 
-meta_train = np.zeros(shape=(len(files), 8))  
-meta_test = np.zeros(shape=(len(outfiles), 8))  
+evallist = [(dtrain, 'train'), (dtest, 'val')]  # 测试 , (dtrain, 'train')
+num_round = 300  # 循环次数
+bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=50)
 
-# print(x_train.shape) # (8, 603)
-# print(y_train.shape) # (8,)
-# print(x_test.shape) # (2, 603)
+# dtr = xgb.DMatrix(train_features)
+pred_val = bst.predict(dtest)
+pred_test = bst.predict(dout)
+result = pred_test
 
-print(x_train.transpose().shape)
+out = []
+for i in range(len(file_names)):
+    tmp = []
+    a = result[i].tolist()
+    # for j in range(len(a)):
+    #     a[j] = ("%.5f" % a[j])
 
-svc = SVC(probability=True, gamma='auto')
-svc.fit(x_train, y_train)
-pred_prob = svc.predict_proba(x_test)
-print(pred_prob)
+    tmp.append(file_names[i])
+    tmp.extend(a)
+    out.append(tmp)
+with open("xgboost_original_para.csv", "w", newline='') as csvfile:
+    writer = csv.writer(csvfile)
 
-
-# StratifiedKFold用法类似 Kfold，但是他是分层采样，确保训练集，测试集中各类别样本的比例与原始数据集中相同
-# skf = StratifiedKFold(n_splits=5, random_state=4, shuffle=True)
-
-# for i, (tr_ind, te_ind) in enumerate(skf.split(x_train, labels)):
-#     X_train, X_train_label = x_train[tr_ind], labels[tr_ind]
-#     X_val, X_val_label = x_train[te_ind], labels[te_ind]
-
-#     print(x_train.shape)
-#     print(x_train)
-#     print(X_train_label.shape)
-#     print(X_train_label)
-#     print(X_val.shape)
-#     print(X_val)
-#     print(X_val_label.shape)
-#     print(X_val_label)
-#     break
-
-#     print('FOLD: {}'.format(str(i)))
-#     print(len(te_ind), len(tr_ind)) # 测试集和训练集数量
-
-#     # dtrain = xgb.DMatrix(X_train, label=X_train_label)
-#     # dtest = xgb.DMatrix(X_val, X_val_label)
-#     # dout = xgb.DMatrix(x_test)
-#     svc = SVC(probability=True, gamma='auto')
-#     svc.fit(X_train, X_train_label)
-#     pred_test = svc.predict(x_test)
-#     print(pred_test)
-#     # pred_prob = svc.decision_function(x_test)
-#     # print(pred_prob)
-#     pred_prob = svc.predict_proba(x_test)
-#     print(pred_prob)
-
-    # dtr = xgb.DMatrix(train_features)
-    # pred_val = bst.predict(dtest)
-    # pred_test = bst.predict(dout)
-    # meta_train[te_ind] = pred_val
-
-#     meta_test += pred_test
-# meta_test /= 5.0
-# result = meta_test
-# # print(result)
-# out = []
-# for i in range(len(file_names)):
-#     tmp = []
-#     a = result[i].tolist()
-#     # for j in range(len(a)):
-#     #     a[j] = ("%.5f" % a[j])
-
-#     tmp.append(file_names[i])
-#     tmp.extend(a)
-#     out.append(tmp)
-# with open("../test/svc.csv", "w", newline='') as csvfile:
-#     writer = csv.writer(csvfile)
-
-#     # 先写入columns_name
-#     writer.writerow(["file_id", "prob0", "prob1", "prob2", "prob3", "prob4", "prob5", "prob6", "prob7"
-#                      ])
-#     # 写入多行用writerows
-#     writer.writerows(out)
+    # 先写入columns_name
+    writer.writerow(["file_id", "prob0", "prob1", "prob2", "prob3", "prob4", "prob5", "prob6", "prob7"
+                     ])
+    # 写入多行用writerows
+    writer.writerows(out)
--- xgboost.py
+++ xgboost.py
@@ -1,11 +1,3 @@
-'''
-特征提取和模型使用整合到同一个文件中，各自完整代码参照其他文件
-采用ngram分词或TFIDF提取特征，范围为（1,3）
-StratifiedKFold 5折交叉验证
-xgboost进行分类
-
-svm进行分类
-'''
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
@@ -17,39 +9,20 @@ import time
 import csv
 
 # test 文件
-with open("../test/security_test.csv.pkl", "rb") as f:
+with open("security_test.csv.pkl", "rb") as f:
     file_names = pickle.load(f)
     outfiles = pickle.load(f)  
 
 # train 文件
-with open("../test/security_train.csv.pkl", "rb") as f:
+with open("security_train.csv.pkl", "rb") as f:
     labels = pickle.load(f)
     files = pickle.load(f)  
-
-train_labels = np.zeros((len(labels), 8), dtype=np.int)
-
-# 标签平滑
-for i in range(len(labels)):
-    for j in range(len(labels[i])):
-        if labels[i][j] == 0:
-            labels[i][j] = 0.005 / 7
-        else:
-            labels[i][j] = 0.995
-
 
 # 词袋模型 ngram 提取特征
 vectorizer = CountVectorizer(ngram_range=(1, 3))
 x_train = vectorizer.fit_transform(files)  # (13887, 180858) 
 y_train = labels # (13887,)
 x_test = vectorizer.transform(outfiles)  # (12955, 180858)
-
-# # Tfidf 提取特征
-# vectorizer = TfidfVectorizer(ngram_range=(1,3), min_df=3, max_df=0.9, )  
-# train_features = vectorizer.fit_transform(files)  # (13887, 180858) 
-# y_train = labels # (13887,)
-# out_features = vectorizer.transform(outfiles)  # (12955, 180858)
-
-
 
 # xgboost 模型
 meta_train = np.zeros(shape=(len(files), 8))  
@@ -83,11 +56,6 @@ for i, (tr_ind, te_ind) in enumerate(skf.split(x_train, labels)):
 meta_test /= 5.0
 result = meta_test
 
-# SVM 模型
-svc = SVC(gamma='auto', probability=True, decision_function_shape='ovo')
-svc.fit(x_train, y_train)
-result = svc.predict_proba(x_test)
-
 out = []
 for i in range(len(file_names)):
     tmp = []
@@ -98,7 +66,7 @@ for i in range(len(file_names)):
     tmp.append(file_names[i])
     tmp.extend(a)
     out.append(tmp)
-with open("../test/ngram_3gram.csv", "w", newline='') as csvfile:
+with open("xgboost.csv", "w", newline='') as csvfile:
     writer = csv.writer(csvfile)
 
     # 先写入columns_name
