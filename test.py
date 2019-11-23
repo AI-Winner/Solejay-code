f0f6d9131dbe8a633269bf656379a75573c5f3ed
Solejay <prj960827@gmail.com>
2019-11-23 14:43:14 +0800

    xgboost保存ngram词向量矩阵

--- test.py
+++ test.py
@@ -1,62 +1,44 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import train_test_split
-import xgboost_model as xgb
-import pickle
-import numpy as np
-import time
-import csv
+# 混淆矩阵代码
+true_train = []
+pred_train = []
+meta_test = np.zeros(shape=(len(outfiles), 8))  
+# StratifiedKFold用法类似 Kfold，但是他是分层采样，确保训练集，测试集中各类别样本的比例与原始数据集中相同
+skf = StratifiedKFold(n_splits=5, random_state=0, shuffle=True)
+for i, (tr_ind, te_ind) in enumerate(skf.split(x_train, labels)):
+    X_train, X_train_label = x_train[tr_ind], labels[tr_ind]
+    X_val, X_val_label = x_train[te_ind], labels[te_ind]
 
-# test 文件
-with open("security_test.csv.pkl", "rb") as f:
-    file_names = pickle.load(f)
-    outfiles = pickle.load(f)  
+    
+    meta_train = np.zeros(shape=(len(tr_ind), 8))
+    meta_val = np.zeros(shape=(len(te_ind), 8))
 
-# train 文件
-with open("security_train.csv.pkl", "rb") as f:
-    labels = pickle.load(f)
-    files = pickle.load(f)  
+    print('FOLD: {}'.format(str(i)))
+    print(len(te_ind), len(tr_ind)) # 测试集和验证集数量
 
-# 词袋模型 ngram 提取特征
-vectorizer = CountVectorizer(ngram_range=(1, 3))
-x = vectorizer.fit_transform(files)  # (13887, 180858)
-y = labels # (13887,)
-x_test = vectorizer.transform(outfiles)  # (12955, 180858)
+    dtrain = xgb.DMatrix(X_train, label=X_train_label)
+    dtest = xgb.DMatrix(X_val, X_val_label)
+    dout = xgb.DMatrix(x_test)
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-dtrain = xgb.DMatrix(X_train, label=y_train)
-dtest = xgb.DMatrix(X_test, y_test)
-dout = xgb.DMatrix(x_test)
+    param = {'max_depth': 6, 'eta': 0.1, 'eval_metric': 'mlogloss', 'silent': 1, 'objective': 'multi:softprob',
+             'num_class': 8, 'subsample': 1,
+             'colsample_bytree': 0.85}  # 参数
 
-param = {'eval_metric': 'mlogloss', 'objective': 'multi:softprob',
-         'num_class': 8}  # 参数
+    evallist = [(dtrain, 'train'), (dtest, 'val')]  # 测试 , (dtrain, 'train')
+    num_round = 10  # 循环次数
+    bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=50)
 
-evallist = [(dtrain, 'train'), (dtest, 'val')]  # 测试 , (dtrain, 'train')
-num_round = 300  # 循环次数
-bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=50)
+    # dtr = xgb.DMatrix(train_features)
+    pred_val = bst.predict(dtest)
 
-# dtr = xgb.DMatrix(train_features)
-pred_val = bst.predict(dtest)
-pred_test = bst.predict(dout)
-result = pred_test
+    # 验证集真实值和预测值
+    true_val = X_val_label
+    pred_val = pred_val.argmax(axis=1)
 
-out = []
-for i in range(len(file_names)):
-    tmp = []
-    a = result[i].tolist()
-    # for j in range(len(a)):
-    #     a[j] = ("%.5f" % a[j])
+    true_train = np.append(true_train, true_val) # 验证集真实标签求和
+    print(true_train.shape)
+    pred_train = np.append(pred_train, pred_val) # 验证集预测标签求和
+    print(pred_train.shape)
 
-    tmp.append(file_names[i])
-    tmp.extend(a)
-    out.append(tmp)
-with open("xgboost_original_para.csv", "w", newline='') as csvfile:
-    writer = csv.writer(csvfile)
+    print(confusion_matrix(pred_val, true_val))
 
-    # 先写入columns_name
-    writer.writerow(["file_id", "prob0", "prob1", "prob2", "prob3", "prob4", "prob5", "prob6", "prob7"
-                     ])
-    # 写入多行用writerows
-    writer.writerows(out)
+print(confusion_matrix(pred_train, true_train))
---
+++ xgboost.py
@@ -0,0 +1,88 @@
+# 读取训练集和测试集
+from sklearn.metrics import confusion_matrix
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import StratifiedKFold
+import xgboost as xgb
+import pickle
+import numpy as np
+import time
+import csv
+
+# train 文件
+with open("security_train.csv.pkl", "rb") as f:
+    labels = pickle.load(f) # ndarray (13887,)
+    train_apis = pickle.load(f)  # list 13887
+
+# test 文件
+with open("security_test.csv.pkl", "rb") as f:
+    test_nums = pickle.load(f) # list 12955
+    test_apis = pickle.load(f) # list 12955
+
+# 词袋模型 ngram 提取特征
+vectorizer = CountVectorizer(ngram_range=(1, 3))
+x_train = vectorizer.fit_transform(train_apis)  # (13887, 180858) 
+y_train = labels # (13887,)
+x_test = vectorizer.transform(test_apis)  # (12955, 180858)
+
+# 将词向量保存下来
+with open("ngram_model/ngram(1,3)_vec.pkl", 'wb') as f:
+    pickle.dump(x_train, f)
+    pickle.dump(y_train, f)
+    pickle.dump(x_test, f)
+    pickle.dump(test_nums, f)
+
+# 加载词向量
+with open("ngram_model/ngram(1,3)_vec.pkl", 'rb') as f:
+    x_train = pickle.load(f) # (13887, 180858) 
+    y_train = pickle.load(f) # (13887,)
+    x_test = pickle.load(f) # (12955, 180858)
+    test_nums = pickle.load(f) # list 12955
+
+# xgboost对词向量模型进行分类
+ngram_train = np.zeros(shape=(13887, 8))  # 训练集（验证集）预测结果
+ngram_test = np.zeros(shape=(12955, 8)) # 测试集预测结果
+
+skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+for i, (tr_ind, te_ind) in enumerate(skf.split(x_train, y_train)):
+    X_train, X_train_label = x_train[tr_ind], y_train[tr_ind]
+    X_val, X_val_label = x_train[te_ind], y_train[te_ind]
+
+    print('FOLD: {}'.format(str(i)))
+    print(len(te_ind), len(tr_ind)) # 测试集和训练集数量
+
+    dtrain = xgb.DMatrix(X_train, label=X_train_label)
+    dtest = xgb.DMatrix(X_val, X_val_label)
+    dout = xgb.DMatrix(x_test)
+
+    param = {'max_depth': 6, 'eta': 0.1, 'eval_metric': 'mlogloss', 'silent': 1, 'objective': 'multi:softprob',
+             'num_class': 8, 'subsample': 1,
+             'colsample_bytree': 0.85}  # 参数
+
+    evallist = [(dtrain, 'train'), (dtest, 'val')]  # 测试 , (dtrain, 'train')
+    num_round = 50  # 循环次数
+    bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=50)
+
+    # dtr = xgb.DMatrix(train_features)
+    pred_val = bst.predict(dtest)
+    pred_test = bst.predict(dout)
+    ngram_train[te_ind] = pred_val
+    ngram_test += pred_test
+ngram_test /= 5.0
+result = ngram_test
+
+out = []
+for i in range(len(test_nums)):
+    tmp = []
+    a = result[i].tolist()
+
+    tmp.append(test_nums[i])
+    tmp.extend(a)
+    out.append(tmp)
+with open("xgboost(1,3)测试123.csv", "w", newline='') as csvfile:
+    writer = csv.writer(csvfile)
+
+    # 先写入columns_name
+    writer.writerow(["file_id", "prob0", "prob1", "prob2", "prob3", "prob4", "prob5", "prob6", "prob7"
+                     ])
+    # 写入多行用writerows
+    writer.writerows(out)
